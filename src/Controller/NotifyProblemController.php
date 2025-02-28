686b13eadfe55fb5d0a26d6fa08a5e3458c2875b
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2025-02-28 17:21:40 +0100

    Changer: envois des mails de notifications à assistance ou si un utilisateur est assigné à l'application, à celui-ci

--- src/Controller/NotifyProblemController.php
+++ src/Controller/NotifyProblemController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Form\NotifyProblemFormType;
 use App\Model\NotifyProblem;
+use App\Repository\ApplicationRepository;
+use Exception;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,6 +17,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class NotifyProblemController extends AbstractController
 {
+    public function __construct(private ApplicationRepository $applicationRepository) {}
+
     /**
      * @throws TransportExceptionInterface
      */
@@ -22,50 +26,39 @@ class NotifyProblemController extends AbstractController
     public function index(Request $request, MailerInterface $mailer): Response
     {
         $problem = new NotifyProblem();
+
         $form = $this->createForm(NotifyProblemFormType::class, $problem);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $application = $this->applicationRepository->findOneBy(['title' => $problem->title]);
 
-            $text = "Problème signalé sur l'application" . " " . $problem->title . PHP_EOL;
-            $text .= ($problem->message == '') ? "Aucune description renseignée" : $problem->message . PHP_EOL;
+            if ($application == null)
+                throw new Exception("Veuillez contacter la DSIUN-PAS, l'application n'existe pas en Base de données");
+
+            $notifyUser = $application->getUser();
+
+            $text = "L'utilisateur ayant pour identifiant: " . $this->getUser()->getUserIdentifier() . " a signalé un disfonctionnement sur l'application : " . $problem->title . PHP_EOL . PHP_EOL;
+            $text .= "Description renseignée par l'utilisateur : ". PHP_EOL . $problem->message . PHP_EOL;
 
             $subject = "Méteo des services - équipe PAS - " . $problem->title;
 
             $email = (new TemplatedEmail())
+                ->from('no-reply@univ-paris1.fr')
                 ->subject($subject)
                 ->htmlTemplate('emails/email_template.html.twig')
                 ->context([
                     'subject' => $subject,
-                    'message' => $text
+                    'message' => $text,
                 ]);
 
-            $toSend = false;
-            $env = $this->getParameter('kernel.environment');
+            if ($notifyUser)
+                $email->to($notifyUser->getMail());
+            else
+                $email->to('assistance-dsiun@univ-paris1.fr');
 
-            if ($env == "dev" || $env == "test") {
-                $from = $this->getUser()->getUserIdentifier() ."@univ-paris1.fr";
-
-                $email->from(new Address($from, "Méteo des Services " . strtoupper($env)));
-
-                if ($this->isGranted('ROLE_SUPER_ADMIN')) {
-                    $toSend = true;
-                    $email->to($from);
-                } else {
-                    $this->addFlash("error", "Application en test pas d'envoi de mails");
-                }
-            } else {
-                throw new \Exception("Environnement autre que dev/test non configurés");
-            }
-
-            if ($toSend) {
-                try {
-                    $mailer->send($email);
-                    $this->addFlash('success', 'Problème signalé avec succès');
-                }
-                catch (TransportExceptionInterface $e) {
-                    $this->addFlash('error', 'Erreur lors de la transmission de votre signalement : ' . $e->getMessage());
-                }
-            }
+            $mailer->send($email);
+            $this->addFlash('success', 'Problème signalé avec succès');
             return $this->redirectToRoute('app_meteo');
         } else {
             return $this->render('notify_problem/index.html.twig', [
