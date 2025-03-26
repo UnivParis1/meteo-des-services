9ab02d7a37f8d54c01d5472241b6313f8d2c5602
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2025-03-26 15:42:22 +0100

    Changer: roles utilisateur: séléction définie (user, admin, super_admin) à la place d'un champs texte libre

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,13 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Form\UserType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 #[IsGranted('ROLE_SUPER_ADMIN')]
 class UserCrudController extends AbstractCrudController
@@ -31,7 +33,11 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('uuid');
-        yield ArrayField::new('roles');
+        yield ChoiceField::new('roles')->setFormType(ChoiceType::class)
+                                       ->setFormTypeOption("expanded", false)
+                                       ->setFormTypeOption("multiple", true)
+                                       ->setFormTypeOption('mapped', true)
+                                       ->setChoices(["user" => 'ROLE_USER', "admin" => 'ROLE_ADMIN', 'super_admin' => "ROLE_SUPER_ADMIN"]);
         yield TextField::new('displayName');
         yield TextField::new('mail');
         yield BooleanField::new('recevoirMail');
