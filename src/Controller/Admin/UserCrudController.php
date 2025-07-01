1cd0b46d6599de81bf3a3ab6b813336323ebfa5f
Etienne Bohm <etienne.bohm@univ-paris1.fr>
2025-07-01 17:36:07 +0200

    Enlever: correction mineure: helper concernant un bug résolu dans la partie gestion utilisateurs du backoffice

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,13 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use App\Form\UserType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,15 +17,6 @@ class UserCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return User::class;
-    }
-
-    public function configureCrud(Crud $crud): Crud
-    {
-        $crud = parent::configureCrud($crud);
-
-        $crud->setHelp("edit", "Pour le champs applications, on peut rajouter des applications à l'utilisateur mais non lui en enlever. Pour enlever des applications, le seul moyen (bug), c'est de supprimer complétement l'utilisateur");
-
-        return $crud;
     }
 
     public function configureFields(string $pageName): iterable
